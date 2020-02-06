f337c1d3ed6618f22877eec3a67d58300ba73a72
hhefesto <hhefesto@rdataa.com>
2020-02-06 13:39:43 -0600

    tons of changes.

---
+++ cachix/komposition.nix
@@ -0,0 +1,13 @@
+
+{
+  nix = {
+    binaryCaches = [
+      "https://komposition.cachix.org"
+    ];
+    binaryCachePublicKeys = [
+      "komposition.cachix.org-1:nzWESzP0bEENshGnqQYN8+mic6JOxw2APw/AJAXhF3Y="
+    ];
+    
+  };
+}
+    
--- configuration.nix
+++ configuration.nix
@@ -3,11 +3,13 @@
 # and in the NixOS manual (accessible by running ‘nixos-help’).
 
 { config, pkgs, ... }:
-
-{
+let
+  nixpkgs-19-03 = import (fetchTarball https://releases.nixos.org/nixos/19.03/nixos-19.03.173684.c8db7a8a16e/nixexprs.tar.xz) { };
+in {
   imports =
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
+      ./cachix.nix
     ];
 
   # Use the systemd-boot EFI boot loader.
@@ -35,85 +37,111 @@
   #   TERMINAL = [ "st" ];
   #   OH_MY_ZSH = [ "${pkgs.oh-my-zsh}/share/oh-my-zsh" ];
   # };
+  
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   nixpkgs.config.allowUnfree = true;
-  # nixpkgs.config = {
-  #   allowUnfree = true;
-  #   st.conf = builtins.readFile /home/hhefesto/src/st/config.def.h;
-  # };
 
+  environment.interactiveShellInit = ''
+    alias gs='git status'
+    alias sendmail='/run/current-system/sw/bin/msmtp --debug --from=default --file=/etc/msmtp/laurus -t'
+    alias xclip='xclip -selection c'
+    alias please='sudo'
+  '';
 
-  # nixpkgs.overlays = [ (self: super: {
-  #   st = super.st.override {
-  #     patches = builtins.map super.fetchurl [
-  #         # { url = "http://st.suckless.org/patches/st-no_bold_colors-20160727-308bfbf.diff";
-  #         #   sha256 = "2e8cdbeaaa79ed067ffcfdcf4c5f09fb5c8c984906cde97226d4dd219dda39dc";
-  #         # }
-  #         # { url = "http://st.suckless.org/patches/st-solarized-both-20160727-308bfbf.diff";
-  #         #   sha256 = "b7b25ba01b7ae87ec201ebbb1bf82742d5979788ecc6773a356eebe7d90a9703";
-  #         # }
-  #         # { url = "https://raw.githubusercontent.com/hhefesto/dotfiles/master/patches/st-scrollback-20190331-21367a0.diff";
-  #         #   sha1 = "fc5140eb0cc74636e5a0f5cd629e3cfbd10c9ed7";
-  #         # }
-  #         # { url = "https://raw.githubusercontent.com/hhefesto/dotfiles/master/patches/st-solarized-both-0.8.1.diff";
-  #         #   sha256 = "be003c27ad96ff82a90353455fa6dbd50b5030ecbb751b6ed7283ed0023e683d";
-  #         # }
-  #         { url = "https://st.suckless.org/patches/scrollback/st-scrollback-0.7.diff";
-  #           sha256 = "f721b15a5aa8d77a4b6b44713131c5f55e7fca04006bc0a3cb140ed51c14cfb6";
-  #         }
-  #         # { url = "https://st.suckless.org/patches/scrollback/st-scrollback-mouse-0.8.diff";
-  #         #   sha1 = "46e92d9d3f6fd1e4f08ed99bda16b232a1687407";
-  #         # }
-  #         # { url = "https://st.suckless.org/patches/scrollback/st-scrollback-mouse-altscreen-0.8.diff";
-  #         #   sha1 = "d3329413998c5f3feaa7764db36269bf7b3d1334";
-  #         # }
-  #       ];
-  #   };
-  # }) ];
-  environment.systemPackages = with pkgs; [
-    # (import (fetchGit "https://github.com/haslersn/fish-nix-shell"))
-    wget
-    vim
-    emacs
-    tmux
-    curl
-    gist
-    git
-    scrot
-    xclip
-    feh
-    firefox
-    dmenu
-    tabbed
-    st
-    haskellPackages.xmobar
-    ranger
-    # fish
-    obs-studio
-    qbittorrent
-    libreoffice
-    vlc
-    dropbox-cli
-    gnome3.nautilus
-    # zsh
-    # oh-my-zsh
-    calibre
-    taffybar
-    sshpass
-    gimp
-    gparted
-    octave
-    htop
-    stack
-    postgresql_11
-    nixops
-    # skypeforlinux
-    google-chrome
-    # spotify # this loops `nixos-rebuild switch` 
-    stack2nix
-    ghc
-  ];
+  # For a Purescript enviroment
+  # let easy-ps = import (pkgs.fetchFromGitHub {
+  #       owner = "justinwoo";
+  #       repo = "easy-purescript-nix";
+  #       rev = "7d072cef5ad9dc33a9a9f1b7fcf8ff083ff484b3";
+  #       sha256 = "0974wrnp8rnmj1wzaxwlmk5mf1vxdbrvjc1h8jgm9j5686qn0mna";
+  #     }) {
+  #       inherit pkgs;
+  #     };
+  # in
+    environment.systemPackages = with pkgs; [
+      # texlive.combine {
+      #   inherit (texlive) scheme-small collection-langkorean algorithms cm-super;
+      # }
+      # easy-ps
+      any-nix-shell
+      texlive.combined.scheme-basic
+      rxvt_unicode
+      wget
+      vim
+      emacs
+      tmux
+      curl
+      gist
+      git
+      lambda-mod-zsh-theme
+      oh-my-zsh
+      zsh
+      scrot
+      xclip
+      feh
+      firefox
+      dmenu
+      tabbed
+      st
+      haskellPackages.xmobar
+      ranger
+      # fish
+      obs-studio
+      qbittorrent
+      libreoffice
+      vlc
+      dropbox-cli
+      gnome3.nautilus
+      calibre
+      taffybar
+      sshpass
+      gimp
+      gparted
+      octave
+      htop
+      nixpkgs-19-03.stack
+      postgresql_11
+      nixops
+      # skypeforlinux
+      google-chrome
+      # spotify # this loops `nixos-rebuild switch` 
+      # stack2nix
+      # ghc
+      ffmpeg
+      xdotool
+      cabal2nix
+      cabal-install
+      nix-prefetch-git
+      xvkbd
+      haskellPackages.yesod-bin
+      # system-sendmail
+      msmtp
+      aspell
+      aspellDicts.en
+      aspellDicts.en-computers
+      aspellDicts.en-science
+      aspellDicts.es
+      inkscape
+      # haskellPackages.keter
+      # nixos.zathura
+      unrar
+      unzip
+      teamviewer
+      hack-font
+      cachix
+      tree
+      gnumake
+      nodejs
+      nodePackages.yarn
+      nixpkgs-19-03.yarn2nix
+      nodePackages.typescript
+      nodePackages.create-react-app
+    ];
+
+fonts.fonts = with pkgs; [
+  hack-font
+];
 
   systemd.user.services.dropbox = {
     restartIfChanged = true;
@@ -123,8 +151,32 @@
       PassEnvironment = "DISPLAY";
     };
   };
+  systemd.user.services."urxvtd" = {
+    enable = true;
+    description = "rxvt unicode daemon";
+    wantedBy = [ "default.target" ];
+    path = [ pkgs.rxvt_unicode ];
+    serviceConfig.Restart = "always";
+    serviceConfig.RestartSec = 2;
+    serviceConfig.ExecStart = "${pkgs.rxvt_unicode}/bin/urxvtd -q -o";
+  };
 
-  
+  programs.zsh = {
+    enable = true;
+    enableCompletion = true;
+    autosuggestions.enable = true;
+    interactiveShellInit = ''
+      # z - jump around
+      # source ${pkgs.fetchurl {url = "https://github.com/rupa/z/raw/2ebe419ae18316c5597dd5fb84b5d8595ff1dde9/z.sh"; sha256 = "0ywpgk3ksjq7g30bqbhl9znz3jh6jfg8lxnbdbaiipzgsy41vi10";}}
+      export ZSH=${pkgs.oh-my-zsh}/share/oh-my-zsh
+      export ZSH_THEME="bira" #"lambda"
+      plugins=(git sudo colorize extract history postgres)
+      source $ZSH/oh-my-zsh.sh
+    '';
+    promptInit = ''
+      any-nix-shell zsh --info-right | source /dev/stdin
+  '';
+  };
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
@@ -150,8 +202,8 @@
   # services.openssh.enable = true;
 
   # Open ports in the firewall.
-  networking.firewall.allowedTCPPorts = [ 3000 5432 ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
+  networking.firewall.allowedTCPPorts = [ 3000 5432 587 5938 ];
+  networking.firewall.allowedUDPPorts = [ 5938 ];
   # Or disable the firewall altogether.
   # networking.firewall.enable = false;
 
@@ -171,7 +223,101 @@
     xserver = {
       enable = true;
       layout = "us";
-      displayManager.gdm.enable = true;
+      xkbOptions = "ctrl:nocaps";
+      xkbVariant = "altgr-intl";
+      displayManager = {
+        gdm.enable = true;
+        gdm.autoLogin.user = "hhefesto";
+        # kdm.enable = false;
+        # lightdm.enable = true;
+        slim.enable = false;
+        slim.defaultUser = "hhefesto";
+        sessionCommands = ''
+          ${pkgs.xorg.xrdb}/bin/xrdb -merge <${pkgs.writeText "Xresources" ''
+             *international: true
+             URxvt*.scrollBar:      false
+             URxvt*.depth:          32
+             URxvt*font:            xft:Source Code Pro:size=10, xft:DejaVu Sans:size=8, xft:Free Mono:size=10
+             URxvt*perl-lib:        /etc/nixos/conf/urxvt-perl
+             URxvt*perl-ext-common: default,clipboard,url-select,keyboard-select
+             URxvt.keysym.Control-f: perl:keyboard-select:search
+             URxvt.keysym.Control-s: perl:keyboard-select:activate
+             URxvt.keysym.Mod1-u: perl:url-select:select_next
+             URxvt.keysym.Mod1-x: perl:clipboard:copy
+             URxvt.keysym.Mod1-y: perl:clipboard:paste
+             URxvt.url-select.launcher: xdg-open
+             URxvt.url-select.underline: true
+
+
+             ! Solarized color scheme for the X Window System
+             !
+             ! http://ethanschoonover.com/solarized
+
+
+             ! Common
+
+             #define S_yellow        #b58900
+             #define S_orange        #cb4b16
+             #define S_red           #dc322f
+             #define S_magenta       #d33682
+             #define S_violet        #6c71c4
+             #define S_blue          #268bd2
+             #define S_cyan          #2aa198
+             #define S_green         #859900
+
+
+             ! Dark
+
+             #define S_base03        #002b36
+             #define S_base02        #073642
+             #define S_base01        #586e75
+             #define S_base00        #657b83
+             #define S_base0         #839496
+             #define S_base1         #93a1a1
+             #define S_base2         #eee8d5
+             #define S_base3         #fdf6e3
+
+
+             ! Light
+
+             !#define S_base03        #fdf6e3
+             !#define S_base02        #eee8d5
+             !#define S_base01        #93a1a1
+             !#define S_base00        #839496
+             !#define S_base0         #657b83
+             !#define S_base1         #586e75
+             !#define S_base2         #073642
+             !#define S_base3         #002b36
+
+             URxvt*background:         S_base03
+             URxvt*foreground:         S_base0
+             URxvt*cursorColor:             S_base1
+             URxvt*pointerColorBackground:  S_base01
+             URxvt*pointerColorForeground:  S_base1
+             URxvt.intensityStyles: false
+
+             URxvt*color0:                  S_base02
+             URxvt*color1:                  S_red
+             URxvt*color2:                  S_green
+             URxvt*color3:                  S_yellow
+             URxvt*color4:                  S_blue
+             URxvt*color5:                  S_magenta
+             URxvt*color6:                  S_cyan
+             URxvt*color7:                  S_base2
+             URxvt*color8:                  S_base03
+             URxvt*color9:                  S_orange
+             URxvt*color10:                 S_base01
+             URxvt*color11:                 S_base00
+             URxvt*color12:                 S_base0
+             URxvt*color13:                 S_violet
+             URxvt*color14:                 S_base1
+             URxvt*color15: S_base3
+
+            Xcursor.theme: Adwaita
+            Xcursor.size: 16
+          ''}
+        '';
+      };
       desktopManager = {
         gnome3.enable = true;
         default = "gnome3";
@@ -187,23 +333,28 @@
         ];
       };
     };
-    # postgresql = {
-    #   enable = true;
-    #   package = pkgs.postgresql_11;
-    #   enableTCPIP = true;
-    #   authentication = pkgs.lib.mkOverride 10 ''
-    #     local all all trust
-    #     host all all ::1/128 trust
-    #   '';
-    #   initialScript = pkgs.writeText "backend-initScript" ''
-    #     CREATE ROLE analyzer WITH LOGIN PASSWORD 'anapass' CREATEDB;
-    #     CREATE DATABASE aanalyzer_yesod;
-    #     GRANT ALL PRIVILEGES ON DATABASE analyzer TO aanalyzer_yesod;
-    # '';
-    # };
-  };
+    postgresql = {
+      enable = true;
+      package = pkgs.postgresql_11;
+      enableTCPIP = true;
+      authentication = pkgs.lib.mkOverride 10 ''
+        local all all trust
+        host all all ::1/128 trust
+      '';
+      initialScript = pkgs.writeText "backend-initScript" ''
+        CREATE ROLE analyzer WITH LOGIN PASSWORD 'anapass' CREATEDB;
+        CREATE DATABASE aanalyzer_yesod;
+        GRANT ALL PRIVILEGES ON DATABASE analyzer TO aanalyzer_yesod;
 
- # virtualisation.virtualbox.host.enable = true;
+        CREATE ROLE hhefesto WITH LOGIN PASSWORD 'hhefesto' CREATEDB;
+        CREATE DATABASE sandbox;
+        GRANT ALL PRIVILEGES ON DATABASE sandbox TO hhefesto;
+    '';
+    };
+  };
+  
+  virtualisation.docker.enable = true;
+  virtualisation.virtualbox.host.enable = true;
   
   # Enable touchpad support.
   # services.xserver.libinput.enable = true;
@@ -220,24 +371,30 @@
   
 
   users.mutableUsers = false;
-#  users.users.root.initialHashedPassword = "$6$somesaltplease$DsmSOQw5RtwC053qvLM/APD.vgolHe/.mrYOSOTJZTsS2brm0/8ZrQgCaEFczipgiPzY/chyqWbk5HG/GGhFH.";
-  users.users.root.initialHashedPassword = "$6$n4g4i9VHr52IqYY$7DMpovz3Z91gSpnfrGPPw.s1DfbdpyfJnwjv7on4G1gtFCOr0PaWOcJUHREuGzZwKZORnx7SOFAqgyehW.nxz/";
-  users.users.hhefesto.initialHashedPassword = "$6$n4g4i9VHr52IqYY$7DMpovz3Z91gSpnfrGPPw.s1DfbdpyfJnwjv7on4G1gtFCOr0PaWOcJUHREuGzZwKZORnx7SOFAqgyehW.nxz/"; # this may be redundant
+
+  # Password generated with ```mkpasswd -m sha-512```
+  # users.users.root.initialHashedPassword = "$6$n4g4i9VHr52IqYY$7DMpovz3Z91gSpnfrGPPw.s1DfbdpyfJnwjv7on4G1gtFCOr0PaWOcJUHREuGzZwKZORnx7SOFAqgyehW.nxz/";
+  users.users.root.initialHashedPassword = "$6$/RvS0Se.iCx$A0eA/8PzgMj.Ms9ohNamfu53c9S.zdG30hEmUHLjmWP0CaXTPVA6QxGIZ6fy.abkjSOTJMAq7fFL6LUBGs4BU0";
+  users.users.hhefesto.initialHashedPassword = "$6$/RvS0Se.iCx$A0eA/8PzgMj.Ms9ohNamfu53c9S.zdG30hEmUHLjmWP0CaXTPVA6QxGIZ6fy.abkjSOTJMAq7fFL6LUBGs4BU0"; # this may be redundant
+  # users.users.hhefesto.initialHashedPassword = "$6$n4g4i9VHr52IqYY$7DMpovz3Z91gSpnfrGPPw.s1DfbdpyfJnwjv7on4G1gtFCOr0PaWOcJUHREuGzZwKZORnx7SOFAqgyehW.nxz/"; # this may be redundant
+  # users.defaultUserShell = pkgs.zsh;
   users.extraUsers.hhefesto = {
     createHome = true;
     isNormalUser = true;
     home = "/home/hhefesto";
     description = "Daniel Herrera";
-    extraGroups = [ "wheel" "networkmanager" ];
-    hashedPassword = "$6$n4g4i9VHr52IqYY$7DMpovz3Z91gSpnfrGPPw.s1DfbdpyfJnwjv7on4G1gtFCOr0PaWOcJUHREuGzZwKZORnx7SOFAqgyehW.nxz/";
-#    hashedPassword = "$6$somesaltplease$DsmSOQw5RtwC053qvLM/APD.vgolHe/.mrYOSOTJZTsS2brm0/8ZrQgCaEFczipgiPzY/chyqWbk5HG/GGhFH.";
-    shell = "/run/current-system/sw/bin/bash";
+    extraGroups = [ "wheel" "networkmanager" "docker" ];
+    hashedPassword = "$6$/RvS0Se.iCx$A0eA/8PzgMj.Ms9ohNamfu53c9S.zdG30hEmUHLjmWP0CaXTPVA6QxGIZ6fy.abkjSOTJMAq7fFL6LUBGs4BU0";
+    shell = pkgs.zsh; #"/run/current-system/sw/bin/bash";
   };
 
-# This value determines the NixOS release with which your system is to be
+  # texlive.combine {
+  #   inherit (texlive) scheme-small algorithms cm-super;
+  # };
+
+  # This value determines the NixOS release with which your system is to be
   # compatible, in order to avoid breaking some software such as database
   # servers. You should change this only after NixOS release notes say you
   # should.
-  system.stateVersion = "19.03"; # Did you read the comment?
-
+  system.stateVersion = "19.09"; # Did you read the comment?
 }
