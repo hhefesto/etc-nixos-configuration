d97a58f93f89aa2beb3c816d41d36325d41faff6
hhefesto <hhefesto@rdataa.com>
2021-06-03 17:13:13 -0500

    updated iohk binaryCachePublicKeys

--- cachix/iohk.nix
+++ cachix/iohk.nix
@@ -5,8 +5,7 @@
       "https://iohk.cachix.org"
     ];
     binaryCachePublicKeys = [
-      "iohk.cachix.org-1:DpRUyj7h7V830dp/i6Nti+NEO2/nhblbov/8MW7Rqoo="
+      "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ="
     ];
   };
 }
-    
