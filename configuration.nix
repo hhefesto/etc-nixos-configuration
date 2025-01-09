99192b6a743d3608d4952e9f5c901e7e05a3d85f
hhefesto <hhefesto@rdataa.com>
2025-01-09 17:23:20 -0600

    steam installed

--- configuration.nix
+++ configuration.nix
@@ -175,6 +175,13 @@
     hack-font
   ];
 
+  programs.steam = {
+    enable = true;
+    remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
+    dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+    localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
+  };
+
   programs.light.enable = true;
 
   programs.zsh = {
