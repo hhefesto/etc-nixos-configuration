c1f631e4c73c033e112a489417d6fef5170f8210
hhefesto <hhefesto@rdataa.com>
2026-03-25 18:34:17 -0600

    Add flake-parts

--- flake.nix
+++ flake.nix
@@ -1,58 +1,78 @@
 {
   description = "hhefesto's system configuration";
 
-  inputs.determinate.url = "https://flakehub.com/f/DeterminateSystems/determinate/*";
-  inputs.nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/*";
-  inputs.home-manager.url = "github:nix-community/home-manager/release-25.11";
-  inputs.home-manager.inputs.nixpkgs.follows = "nixpkgs";
-  inputs.agenix = {
-    url = "github:ryantm/agenix";
-    inputs.nixpkgs.follows = "nixpkgs";
-  };
-  inputs.claude-code-nix.url = "github:sadjow/claude-code-nix";
-  inputs.spacemacs = {
-    url = "github:syl20bnr/spacemacs";
-    flake = false;
-  };
-
-
-  outputs = inputs@{ self, determinate, nixpkgs, home-manager, claude-code-nix, ... }:
-  let
-    system = "x86_64-linux";
-    pkgs = nixpkgs.legacyPackages.${system};
-    mkHost = { hostModule, xmobarrc, extraSpecialArgs ? {} }: nixpkgs.lib.nixosSystem {
-      inherit system;
-      modules = [ hostModule
-                  ./configuration.nix
-                  # determinate.nixosModules.default
-                  home-manager.nixosModules.home-manager
-                  {
-                    home-manager.useGlobalPkgs = true;
-                    home-manager.useUserPackages = true;
-                    home-manager.users.hhefesto = import ./home.nix;
-                    home-manager.extraSpecialArgs = {
-                      spacemacs = inputs.spacemacs;
-                      inherit xmobarrc;
-                    };
-                  }
-                  {
-                    nixpkgs.overlays = [ claude-code-nix.overlays.default ];
-                  }
-                ];
-      specialArgs = { inherit inputs; } // extraSpecialArgs;
+  inputs = {
+    determinate.url = "https://flakehub.com/f/DeterminateSystems/determinate/*";
+    nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/*";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    home-manager = {
+      url = "github:nix-community/home-manager/release-25.11";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
-  in {
-    nixosConfigurations.delfos = mkHost {
-      hostModule = ./delfos.nix;
-      xmobarrc = ./xmobarrc-delfos;
-      extraSpecialArgs = { xmonadShortenLength = 26; };
+    agenix = {
+      url = "github:ryantm/agenix";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
-
-    nixosConfigurations.olimpo = mkHost {
-      hostModule = ./olimpo.nix;
-      xmobarrc = ./xmobarrc-olimpo;
+    claude-code-nix.url = "github:sadjow/claude-code-nix";
+    spacemacs = {
+      url = "github:syl20bnr/spacemacs";
+      flake = false;
     };
-
-    packages.${system}.default = self.nixosConfigurations.delfos.config.system.build.toplevel;
   };
+
+  outputs = inputs@{ self, flake-parts, nixpkgs, home-manager, claude-code-nix, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [ "x86_64-linux" ];
+
+      flake = let
+        system = "x86_64-linux";
+        mkHost = { hostModule, xmobarrc, extraSpecialArgs ? {} }: nixpkgs.lib.nixosSystem {
+          inherit system;
+          modules = [
+            hostModule
+            ./configuration.nix
+            # determinate.nixosModules.default
+            home-manager.nixosModules.home-manager
+            {
+              home-manager.useGlobalPkgs = true;
+              home-manager.useUserPackages = true;
+              home-manager.users.hhefesto = import ./home.nix;
+              home-manager.extraSpecialArgs = {
+                spacemacs = inputs.spacemacs;
+                inherit xmobarrc;
+              };
+            }
+            {
+              nixpkgs.overlays = [ claude-code-nix.overlays.default ];
+            }
+          ];
+          specialArgs = { inherit inputs; } // extraSpecialArgs;
+        };
+      in {
+        nixosConfigurations.delfos = mkHost {
+          hostModule = ./delfos.nix;
+          xmobarrc = ./xmobarrc-delfos;
+          extraSpecialArgs = { xmonadShortenLength = 26; };
+        };
+
+        nixosConfigurations.olimpo = mkHost {
+          hostModule = ./olimpo.nix;
+          xmobarrc = ./xmobarrc-olimpo;
+        };
+      };
+
+      perSystem = { pkgs, ... }: {
+        packages = let
+          hosts = self.nixosConfigurations;
+          entries = builtins.map (name: {
+            inherit name;
+            path = hosts.${name}.config.system.build.toplevel;
+          }) (builtins.attrNames hosts);
+        in {
+          default = pkgs.linkFarm "all-hosts" entries;
+          delfos = hosts.delfos.config.system.build.toplevel;
+          olimpo = hosts.olimpo.config.system.build.toplevel;
+        };
+      };
+    };
 }
